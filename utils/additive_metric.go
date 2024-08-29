f5d9883c85e25bfd313f7820478511e4ad78af30
Sai Jayanth Kanukolan <19990699+jaysrivatsa@users.noreply.github.com>
2024-08-29 03:27:11 +0000

    Replacing Z computation with function

--- utils/additive_metric.go
+++ utils/additive_metric.go
@@ -2,17 +2,12 @@ package utils
 
 import (
 	"github.com/jaysrivatsa/sample_size_calculator/models"
-	"github.com/montanaflynn/stats"
 	"math"
 )
 
 func SampleSizeAdditive(r models.AdditiveRequest) float64 {
 	var b float64
-	if r.TwoTailed {
-		b = stats.NormPpf(1-r.Alpha/2, 0, 1) + stats.NormPpf(r.Power, 0, 1)
-	} else {
-		b = stats.NormPpf(1-r.Alpha, 0, 1) + stats.NormPpf(r.Power, 0, 1)
-	}
+	b = GetZ(r.TwoTailed, r.Alpha, r.Power)
 	c := r.Avg*(1+r.Mde) - r.Avg
 	sampleSize := 2 * math.Pow(r.Std*b/c, 2)
 	return math.Ceil(sampleSize)
